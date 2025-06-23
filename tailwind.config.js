95bff0404aca164f217509234da0dbf8c99a8ffd
Phoenix1410 <ambsng2018@gmail.com>
2025-06-23 08:24:21 +0530

    Final breath during storm 6

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    // This line tells Tailwind to scan all .html and .js files in the 'public' folder
+    './public/**/*.{html,js}', 
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
